5c3bf22e4ca66ce396883665938ebc8bd849f1b3
Roberto Tyley <roberto.tyley@gmail.com>
2025-11-09 18:38:09 +0000

    It works, huzzah

--- src/main/scala/com/madgag/algo/sorting/kwaymerge/Merge.scala
+++ src/main/scala/com/madgag/algo/sorting/kwaymerge/Merge.scala
@@ -1,29 +1,83 @@
 package com.madgag.algo.sorting.kwaymerge
 
-import scala.math.BigInt.int2bigInt
+import java.lang.Integer.highestOneBit
+import scala.annotation.tailrec
+import scala.math.Ordered.orderingToOrdered
 
 object Merge {
-  def arrayLengthRequiredFor(k: Int): Int = (1 << (k+1).bitLength) - 1
+
+  def leafLineLengthRequiredFor(k: Int): Int = highestOneBit(k - 1) << 1
 
   def mergeIterable[T: Ordering](seqs: Iterable[T]*): Iterable[T] = {
     ???
   }
 
-  def merge[T: Ordering](seqs: Iterator[T]*): Iterator[T] = {
+  def merge[T](seqs: Iterator[T]*)(using ordering: Ordering[T]): Iterator[T] = {
+    case class NodePayload(value: T, seqIndex: Int)
 
-//    val tree: Array[Option[(T, Iterator[T])]] = for {
-//
-//    }
-//
-//    new Iterator[T] {
-//      override def hasNext: Boolean = tree(0).isDefined
-//
-//      override def next(): T = {
-//        val n = tree(0)
-//
-//        ???
-//      }
-//    }
-    ???
+    given Ordering[Option[NodePayload]] =
+      (x: Option[NodePayload], y: Option[NodePayload]) =>
+        (x, y) match {
+          case (Some(x), Some(y)) => ordering.compare(x._1, y._1)
+          case (None, None) => 0
+          case (None, _) => 1
+          case (_, None) => -1
+        }
+
+    val leafLineLength = leafLineLengthRequiredFor(k = seqs.size)
+    val leafLineBaseIndex = leafLineLength - 1
+
+    val tree = Array.ofDim[Option[NodePayload]](leafLineBaseIndex + leafLineLength)
+
+    def leafNodeValueForNextFrom(seq: Iterator[T], seqIndex: Int): Option[NodePayload] =
+      seq.nextOption().map(value => NodePayload(value, seqIndex))
+
+    for ((seq, seqIndex) <- seqs.zipWithIndex) {
+      tree(leafLineBaseIndex + seqIndex) = leafNodeValueForNextFrom(seq, seqIndex)
+    }
+    for (ti <- leafLineBaseIndex + seqs.size until tree.length) {
+      tree(ti) = None
+    }
+
+    def compareAtTreeIndex(ti: Int, a: Option[NodePayload], b: Option[NodePayload]): Option[NodePayload] = {
+      if (a < b)
+        tree(ti) = b ; a
+      else
+        tree(ti) = a ; b
+    }
+
+    def parentOf(treeIndex: Int) = (treeIndex - 1) / 2
+
+    def determineWinnerOf(ti: Int): Option[NodePayload] = if (ti >= leafLineBaseIndex) tree(ti) else {
+      val lhcIndex = (ti * 2) + 1
+      compareAtTreeIndex(ti, determineWinnerOf(lhcIndex), determineWinnerOf(lhcIndex + 1))
+    }
+
+    @tailrec def replayUpFrom(changedChildTreeIndex: Int, newWinnerSentUpFromChild: Option[NodePayload]): Option[NodePayload] = {
+      val ti = parentOf(changedChildTreeIndex)
+      val winner = compareAtTreeIndex(ti, tree(ti), newWinnerSentUpFromChild)
+      if (ti == 0) winner else replayUpFrom(ti, winner)
+    }
+
+    def replayGamesFor(seqIndex: Int) = {
+      val ti = leafLineBaseIndex + seqIndex
+      val leafValue = leafNodeValueForNextFrom(seqs(seqIndex), seqIndex)
+      tree(ti) = leafValue
+      replayUpFrom(ti, leafValue)
+    }
+
+    Iterator.unfold[T, State](Uninitialized) { state =>
+      val winner = state match {
+        case Uninitialized => determineWinnerOf(0)
+        case PreviousWinner(seqIndex) => replayGamesFor(seqIndex)
+      }
+      winner.map(np => np.value -> PreviousWinner(np.seqIndex))
+    }
   }
 }
+
+sealed trait State
+
+case object Uninitialized extends State
+
+case class PreviousWinner(seqIndex: Int) extends State
--- src/test/scala/com/madgag/algo/sorting/kwaymerge/MergeTest.scala
+++ src/test/scala/com/madgag/algo/sorting/kwaymerge/MergeTest.scala
@@ -1,22 +1,35 @@
 package com.madgag.algo.sorting.kwaymerge
 
+import com.madgag.algo.sorting.kwaymerge.Merge.leafLineLengthRequiredFor
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
 
 class MergeTest extends AnyFlatSpec with should.Matchers {
   "Merge" should "merge several sorted lists and merge them into a single sorted list" in {
     // Example from https://en.wikipedia.org/wiki/K-way_merge_algorithm#Merge
-    Merge.mergeIterable(
-      Seq(2, 7, 16),
-      Seq(5, 10, 20),
-      Seq(3, 6, 21),
-      Seq(4, 8, 9)
-    ) shouldBe Seq(2, 3, 4, 5, 6, 7 , 8, 9, 10, 16, 20, 21)
+    Merge.merge(
+      Seq(2, 7, 16).iterator,
+      Seq(5, 10, 20).iterator,
+      Seq(3, 6, 21).iterator,
+      Seq(4, 8, 9).iterator
+    ).toSeq shouldBe Seq(2, 3, 4, 5, 6, 7 , 8, 9, 10, 16, 20, 21)
   }
 
-  it should "use an array of appro size" in {
-    Merge.arrayLengthRequiredFor(2) shouldBe 3
-    Merge.arrayLengthRequiredFor(3) shouldBe 7
-    Merge.arrayLengthRequiredFor(4) shouldBe 7
+  it should "work dammit" in {
+    Merge.merge(
+      Seq('h', 's').iterator,
+      Seq('a', 'r').iterator,
+      Seq('b', 'o').iterator
+    ).toSeq.mkString shouldBe "abhors"
+  }
+
+
+  it should "use a leaf line of appro size" in {
+    leafLineLengthRequiredFor(2) shouldBe 2
+    leafLineLengthRequiredFor(3) shouldBe 4
+    leafLineLengthRequiredFor(4) shouldBe 4
+    leafLineLengthRequiredFor(5) shouldBe 8
+    leafLineLengthRequiredFor(8) shouldBe 8
+    leafLineLengthRequiredFor(9) shouldBe 16
   }
 }
