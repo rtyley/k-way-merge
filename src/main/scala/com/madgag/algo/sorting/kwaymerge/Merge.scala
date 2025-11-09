cb9d4ee35df13af5b0641c550965cb66a21f3eac
Roberto Tyley <roberto.tyley@gmail.com>
2025-11-09 12:36:56 +0000

    Get the right array size...

--- src/main/scala/com/madgag/algo/sorting/kwaymerge/Merge.scala
+++ src/main/scala/com/madgag/algo/sorting/kwaymerge/Merge.scala
@@ -1,7 +1,29 @@
 package com.madgag.algo.sorting.kwaymerge
 
+import scala.math.BigInt.int2bigInt
+
 object Merge {
-  def merge[T: Ordering](seqs: Iterable[T]*): Iterable[T] = {
-    seqs.head
+  def arrayLengthRequiredFor(k: Int): Int = (1 << (k+1).bitLength) - 1
+
+  def mergeIterable[T: Ordering](seqs: Iterable[T]*): Iterable[T] = {
+    ???
+  }
+
+  def merge[T: Ordering](seqs: Iterator[T]*): Iterator[T] = {
+
+//    val tree: Array[Option[(T, Iterator[T])]] = for {
+//
+//    }
+//
+//    new Iterator[T] {
+//      override def hasNext: Boolean = tree(0).isDefined
+//
+//      override def next(): T = {
+//        val n = tree(0)
+//
+//        ???
+//      }
+//    }
+    ???
   }
 }
--- src/test/scala/com/madgag/algo/sorting/kwaymerge/MergeTest.scala
+++ src/test/scala/com/madgag/algo/sorting/kwaymerge/MergeTest.scala
@@ -6,11 +6,17 @@ import org.scalatest.matchers.should
 class MergeTest extends AnyFlatSpec with should.Matchers {
   "Merge" should "merge several sorted lists and merge them into a single sorted list" in {
     // Example from https://en.wikipedia.org/wiki/K-way_merge_algorithm#Merge
-    Merge.merge(
+    Merge.mergeIterable(
       Seq(2, 7, 16),
       Seq(5, 10, 20),
       Seq(3, 6, 21),
       Seq(4, 8, 9)
     ) shouldBe Seq(2, 3, 4, 5, 6, 7 , 8, 9, 10, 16, 20, 21)
+  }
+
+  it should "use an array of appro size" in {
+    Merge.arrayLengthRequiredFor(2) shouldBe 3
+    Merge.arrayLengthRequiredFor(3) shouldBe 7
+    Merge.arrayLengthRequiredFor(4) shouldBe 7
   }
 }
